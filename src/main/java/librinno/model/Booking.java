87ea6535f1e92f40f7c3522a7d5950eaab779157
reconrus <reconrus@yandex.ru>
2018-02-07 10:25:27 +0300

    test 1 and test 3 almost done

--- src/main/java/librinno/model/Booking.java
+++ src/main/java/librinno/model/Booking.java
@@ -4,28 +4,57 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class Booking {
-    public void check_out(int zaregID, int bookID) {
+    public boolean check_out(int zaregID, int bookID) {
         try {
-            Scanner sc = new Scanner(System.in);
-
-            PreparedStatement ps = Database.con.prepareStatement("Update Books set owner=? where id=?");
-            ps.setInt(1, zaregID);
-            ps.setInt(2, bookID);
-            ps.executeUpdate();
-            ps.close();
 
             Statement ps2 = Database.con.createStatement();
             ResultSet rs = ps2.executeQuery("SELECT Type from Users_of_the_library WHERE Card_number=" + zaregID);
             if (rs.next()) {
                 String type = rs.getString("Type");
-                System.out.println(type);
+
+                PreparedStatement setTime = Database.con.prepareStatement("Update Books set time_left=? where id=?");
+
+                ResultSet book = ps2.executeQuery("SELECT owner, is_bestseller from Books WHERE id=" + bookID);
+
+                if(book.next()) {
+
+                    if(book.getInt("owner") != 0) return false;
+
+                    Boolean is_bestseller = book.getBoolean("is_bestseller");
+
+                    book.close();
+
+                    if (type.equals("Student")) {
+
+                        if (is_bestseller)
+                            setTime.setInt(1, 14);
+                        else setTime.setInt(1, 21);
+
+                    } else setTime.setInt(1, 28);
+
+                    setTime.setInt(2, bookID);
+                    setTime.executeUpdate();
+                    setTime.close();
+
+                    PreparedStatement ps = Database.con.prepareStatement("Update Books set owner=? where id=?");
+                    ps.setInt(1, zaregID);
+                    ps.setInt(2, bookID);
+                    ps.executeUpdate();
+                    ps.close();
+
+                    return true;
+                }
+
             }
-            ResultSet users=ps2.executeQuery("SELECT Type from Users_of_the_library WHERE Card_number=?");
+
+            //ResultSet users=ps2.executeQuery("SELECT Type from Users_of_the_library WHERE Card_number=?");
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return false;
     }
 }
