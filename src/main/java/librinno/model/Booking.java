c33135d2bfe623f65a4e5f06acffe12d9c4e1bb4
DmitryKonev <Konev1999D@mail.ru>
2018-02-07 22:45:08 +0300

    Added tests(all are working) and modernised class booking

--- src/main/java/librinno/model/Booking.java
+++ src/main/java/librinno/model/Booking.java
@@ -6,55 +6,81 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Booking {
+    private boolean more_or_equal_than_one_copy_of_one_book = false;
+
+    public boolean check_out_by_author(int zaregID, String author) {
+        try {
+            Statement stmt = Database.con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT Author,id from Books");
+            int id = -1;
+            //in future there will be while,but not know
+            if (rs.next()) {
+                String book_author = rs.getString("Author");
+                if (book_author.equals(author))
+                    id = rs.getInt("id");
+            }
+            if (id != -1)
+                check_out(zaregID, id);
+            else return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
     public boolean check_out(int zaregID, int bookID) {
         try {
-
             Statement ps2 = Database.con.createStatement();
             ResultSet rs = ps2.executeQuery("SELECT Type from Users_of_the_library WHERE Card_number=" + zaregID);
             if (rs.next()) {
                 String type = rs.getString("Type");
-
                 PreparedStatement setTime = Database.con.prepareStatement("Update Books set time_left=? where id=?");
-
-                ResultSet book = ps2.executeQuery("SELECT owner, is_bestseller from Books WHERE id=" + bookID);
-
-                if(book.next()) {
-
-                    if(book.getInt("owner") != 0) return false;
-
+                ResultSet book = ps2.executeQuery("SELECT owner, is_bestseller,is_reference from Books WHERE id=" + bookID);
+                if (book.next()) {
                     Boolean is_bestseller = book.getBoolean("is_bestseller");
-
-                    book.close();
-
                     if (type.equals("Student")) {
-
                         if (is_bestseller)
                             setTime.setInt(1, 14);
                         else setTime.setInt(1, 21);
-
                     } else setTime.setInt(1, 28);
-
                     setTime.setInt(2, bookID);
                     setTime.executeUpdate();
                     setTime.close();
-
+                    if (book.getInt("owner") != 0) {
+                        System.out.println("user with id " + zaregID + " can't take books,because they have finished,come later");
+                        return false;
+                    } else if (more_or_equal_than_one_copy_of_one_book) {
+                        System.out.println("book is already taken by this user");
+                        return false;
+                    }else if(book.getBoolean("is_reference")){
+                        System.out.println("this is reference,impossible to take");
+                        return false;
+                    } else {
+                        //this is for test
+                        ResultSet equal_number = ps2.executeQuery("SELECT owner from Books");
+                         while (equal_number.next()) {
+                        if (equal_number.getInt("owner") == zaregID)
+                            more_or_equal_than_one_copy_of_one_book = true;
+                    }
+                        if(!more_or_equal_than_one_copy_of_one_book)
+                        System.out.println("user with id " + zaregID + " takes a book");
+                        else {
+                            System.out.println("user with id " + zaregID + " can't take a book");
+                            return false;
+                        }
+                    }
                     PreparedStatement ps = Database.con.prepareStatement("Update Books set owner=? where id=?");
                     ps.setInt(1, zaregID);
                     ps.setInt(2, bookID);
                     ps.executeUpdate();
+                    book.close();
                     ps.close();
-
                     return true;
                 }
-
             }
-
-            //ResultSet users=ps2.executeQuery("SELECT Type from Users_of_the_library WHERE Card_number=?");
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return false;
     }
 }
--- src/main/java/librinno/model/Test.java
+++ src/main/java/librinno/model/Test.java
@@ -12,18 +12,22 @@ public class Test {
     private static String press = "Press Enter to continue \n";
     private static Scanner in;
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) throws SQLException {
 
         Database db = new Database();
 
         in = new Scanner(System.in);
-
+//tests
         test_1();
+        test_2();
         test_3();
         test_4();
+        test_5();
+        test_6();
         test_7();
         test_8();
         test_9();
+        test_10();
     }
 
     private static void test_1() throws SQLException {
@@ -36,16 +40,33 @@ public class Test {
         System.out.println("Ilnur tries to check out Touch of Class \n" + press);
         in.nextLine();
 
-        Booking booking=new Booking();
-        if(booking.check_out(1,2)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(1, 2)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
         System.out.println(press + "\n");
+        defaultTable();
         in.nextLine();
     }
 
+    private static void test_2() throws SQLException {
+        defaultTable();
+        System.out.println("Test 2");
+        printUsers();
+        printBooks();
+        System.out.println("Ilnur tries to check out a book of author A\n" + press);
+        in.nextLine();
+        Booking booking = new Booking();
+        if (booking.check_out_by_author(1, "A"))
+            System.out.println("Checking out was successful\n");
+        else
+            System.out.println("Author not found!!");
+        printBooks();
+        defaultTable();
+        System.out.println(press + "\n");
+    }
 
-    private static void test_3() throws SQLException{
+    private static void test_3() throws SQLException {
         defaultTable();
         System.out.println("Test 3");
 
@@ -55,18 +76,17 @@ public class Test {
         System.out.println("Shilov tries to check out Touch of Class \n" + press);
         in.nextLine();
 
-        Booking booking=new Booking();
-        if(booking.check_out(2,2)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(2, 2)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
-
+        defaultTable();
         System.out.println(press + "\n");
         in.nextLine();
     }
 
 
-
-    private static void test_4() throws SQLException{
+    private static void test_4() throws SQLException {
         defaultTable();
         System.out.println("Test 4");
 
@@ -75,41 +95,72 @@ public class Test {
 
         System.out.println("Shilov tries to check out War And Peace  \n" + press);
         in.nextLine();
-        Booking booking=new Booking();
-        if(booking.check_out(2,1)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(2, 1)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
-
+        defaultTable();
         System.out.println("We have noticed the contradiction between test suite and project description.\nTest suite says to set time on 2 weeks but " +
-                "description for 4 weeks for faculty members REGARDLESS the book is best seller. So we decided to choose project description requirments.\n" + press+"\n");
+                "description for 4 weeks for faculty members REGARDLESS the book is best seller. So we decided to choose project description requirments.\n" + press + "\n");
         in.nextLine();
     }
 
-    private  static void test_7() throws SQLException{
+    private static void test_5() throws SQLException {
+        defaultTable();
+        System.out.println("Test 5");
+        printUsers();
+        printBooks();
+        System.out.println("Ilnur,Ilnur1 and Ilnur2 are trying to take book Touch of class,which there are only 2 in library");
+        Booking booking = new Booking();
+        booking.check_out(1, 2);
+        booking.check_out(3, 3);
+        booking.check_out(4, 3);
+        printBooks();
+        defaultTable();
+        System.out.println(press + "\n");
+        in.nextLine();
+    }
+
+    private static void test_6() throws SQLException {
+        defaultTable();
+        System.out.println("Test 6");
+        printUsers();
+        printBooks();
+        Booking booking = new Booking();
+        booking.check_out(1, 2);
+        System.out.println("Ilnur tries to get 2nd copy of book Touch of class");
+        booking.check_out(1, 3);
+        printBooks();
+        defaultTable();
+        System.out.println(press + "\n");
+        in.nextLine();
+    }
+
+    private static void test_7() throws SQLException {
         defaultTable();
         System.out.println("Test 7");
 
         printUsers();
         printBooks();
 
-        System.out.println("Shilov tries to check out Touch of Class  \n" + press );
+        System.out.println("Shilov tries to check out Touch of Class  \n" + press);
         in.nextLine();
 
-        Booking booking=new Booking();
-        if(booking.check_out(2,2)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(2, 2)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
 
         System.out.println("Ilnur tries to check out Touch of Class  \n" + press);
         in.nextLine();
-        if(booking.check_out(1,3)) System.out.println("Checking out was successful \n\n");
+        if (booking.check_out(1, 3)) System.out.println("Checking out was successful \n\n");
         printBooks();
-
+        defaultTable();
         System.out.println(press + "\n");
         in.nextLine();
     }
 
-    private  static void test_8() throws SQLException{
+    private static void test_8() throws SQLException {
         defaultTable();
         System.out.println("Test 8");
 
@@ -119,80 +170,90 @@ public class Test {
         System.out.println("Ilnur tries to check out Touch of Class  \n" + press);
         in.nextLine();
 
-        Booking booking=new Booking();
-        if(booking.check_out(1,2)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(1, 2)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
-
+        defaultTable();
         System.out.println(press + "\n");
         in.nextLine();
 
     }
 
-    private static void test_9() throws SQLException{
+    private static void test_9() throws SQLException {
         defaultTable();
         System.out.println("Test 9");
-
         printUsers();
         printBooks();
-
         System.out.println("Ilnur tries to check out War And Peace  \n" + press);
         in.nextLine();
-
-        Booking booking=new Booking();
-        if(booking.check_out(1,1)) System.out.println("Checking out was successful \n\n");
+        Booking booking = new Booking();
+        if (booking.check_out(1, 1)) System.out.println("Checking out was successful \n\n");
 
         printBooks();
-
+        defaultTable();
         System.out.println(press + "\n");
         in.nextLine();
     }
 
+    private static void test_10() throws SQLException {
+        defaultTable();
+        System.out.println("Test 10");
+        printUsers();
+        printBooks();
+        System.out.println("Ilnur tries to take Touch of class(not reference and Harry Potter(reference))");
+        Booking booking = new Booking();
+        booking.check_out(1, 2);
+        booking.check_out(1, 4);
+        printBooks();
+        defaultTable();
+        System.out.println(press + "\n");
+        in.nextLine();
+    }
 
-
-    private static void printBooks() throws SQLException{
+    private static void printBooks() throws SQLException {
         Statement ps2 = Database.con.createStatement();
         ResultSet rs = ps2.executeQuery("SELECT * from Books");
 
         System.out.println("Books \n");
-        System.out.println("id       Name                Author          Is Bestseller       Owner       Days left");
+        System.out.println("id       Name                Author          Is Bestseller       Owner       Days left    Is_reference");
 
-        while(rs.next()){
+        while (rs.next()) {
             int id = rs.getInt(1);
             String name = rs.getString(2);
             String author = rs.getString(3);
             String isBestseller;
             int owner = rs.getInt(8);
-            if(rs.getInt(9) == 0) isBestseller = "False";
+            if (rs.getInt(9) == 0) isBestseller = "False";
             else isBestseller = "True";
             int days = rs.getInt(10);
-
-            System.out.println(id + "       " + name +"       " + author + "          " + isBestseller+"                " + owner+"             "+days);
+            Boolean is_reference = rs.getBoolean(11);
+            System.out.println(id + "       " + name + "       " + author + "          " + isBestseller + "                " + owner + "             " + days + "      " + is_reference);
         }
         System.out.println("\n");
     }
 
 
-    private static void printUsers() throws SQLException{
+    private static void printUsers() throws SQLException {
         Statement ps2 = Database.con.createStatement();
         ResultSet rs = ps2.executeQuery("SELECT * from Users_of_the_library");
 
         System.out.println("Users in the system \n");
         System.out.println("ID       Name       Type");
 
-        while(rs.next()){
+        while (rs.next()) {
             int id = rs.getInt(4);
             String name = rs.getString(1);
             String type = rs.getString(5);
 
-            System.out.println(id + "       " + name +"       " + type);
+            System.out.println(id + "       " + name + "       " + type);
         }
 
         System.out.println("\n");
     }
 
 
-    private static void defaultTable() throws SQLException{
+    private static void defaultTable() throws SQLException {
 
         PreparedStatement set = Database.con.prepareStatement("Update Books set time_left=?, owner=?");
         set.setInt(1, 999);
