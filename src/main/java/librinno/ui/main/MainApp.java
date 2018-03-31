c9aa0d5045496e84d183f4a38086ef90bd1cef09
DeadmanIQ445 <solovov305@gmail.com>
2018-03-31 14:24:16 +0300

    making program remember db info

--- src/main/java/librinno/ui/main/MainApp.java
+++ src/main/java/librinno/ui/main/MainApp.java
@@ -8,6 +8,13 @@ import javafx.stage.Stage;
 import main.java.librinno.model.Database;
 import main.java.librinno.model.Main;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
@@ -27,15 +34,31 @@ public class MainApp extends Application {
 
     }
 
-    public static void main(String[] args) {
-        String login, pass;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Write your login in MySQL:");
-        Main.setUSER(sc.nextLine());
-        System.out.println("Write your password in MySQL:");
-        Main.setPASS(sc.nextLine());
-        Database db=new Database();
-        db.creationLocalDB(Main.getUSER(),Main.getPASS());
+    public static void main(String[] args) throws IOException {
+        Path filePath_1= Paths.get("dbinf.txt");
+        if (!Files.exists(filePath_1)) {
+            String login, pass;
+            FileWriter wr = new FileWriter("dbinf.txt");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Write your login in MySQL:");
+            String temp = sc.nextLine();
+            wr.write(temp);
+            wr.write("\n");
+            Main.setUSER(temp);
+            System.out.println("Write your password in MySQL:");
+            temp = sc.nextLine();
+            wr.write(temp);
+            wr.close();
+            Main.setPASS(temp);
+        }
+        else{
+            Scanner sc = new Scanner(new File("dbinf.txt"));
+            Main.setUSER(sc.nextLine());
+            Main.setPASS(sc.nextLine());
+            sc.close();
+        }
+        Database db = new Database();
+        db.creationLocalDB(Main.getUSER(), Main.getPASS());
         launch(args);
 
     }
