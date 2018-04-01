02a79a50a889b58c46d143cd4e7c6b47c6a483bc
DmitryKonev <Konev1999D@mail.ru>
2018-04-01 18:42:48 +0300

    Finally, i did it,now sending to everyone messages

--- src/main/java/librinno/model/SendEmail.java
+++ src/main/java/librinno/model/SendEmail.java
@@ -1,20 +1,18 @@
 package main.java.librinno.model;
 
 import java.sql.*;
-import java.util.Properties;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class SendEmail extends Database{
+public class SendEmail extends Database {
     private static String USER_NAME = "librinno";
     private static String PASSWORD = "thebestteamever";
+    private static ArrayList<Address> all_emails = new ArrayList<>();
 
     public static void send() {
         String from = USER_NAME;
@@ -34,16 +32,18 @@ public class SendEmail extends Database{
         props.put("mail.smtp.password", pass);
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
-
+        Address array[] = null;
         Session session = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(session);
-
+        Transport transport = null;
         try {
-            message.setFrom(new InternetAddress(from));
+                message.setFrom(new InternetAddress(from));
             InternetAddress[] toAddress = new InternetAddress[to.length];
             for (int i = 0; i < to.length; i++) {
-                toAddress[i] = new InternetAddress(to[i]);
+                    toAddress[i] = new InternetAddress(to[i]);
+
             }
+            for (int i = 0; i < toAddress.length; i++) {
                 try {
                     DatabaseMetaData md = con.getMetaData();
                     Statement stmt = con.createStatement();
@@ -51,42 +51,71 @@ public class SendEmail extends Database{
                     while (rs.next()) {
                         String table_name = rs.getString(3);
                         ResultSet table_rs = stmt.executeQuery("SELECT * FROM " + table_name + " LIMIT 1");
-                        for (int i = 0; i < toAddress.length; i++) {
-                            if (table_rs.next() && emailsEquals(table_rs.getString("Email"), toAddress[i].toString()) && table_rs.getBoolean("is_sended") == false) {
+                        while (table_rs.next()) {
+                            String email = table_rs.getString("Email");
+                            String address = toAddress[i].toString().toLowerCase();
+                            boolean bool = table_rs.getInt("is_sended") == 0;
+                            if (emailsEquals(email, address) && bool) {
+                                PreparedStatement pr = con.prepareStatement("UPDATE " + table_name + " SET is_sended=? LIMIT 1");
+                                pr.setInt(1, 1);
+                                pr.executeUpdate();
+                                //косяк!!!!
                                 message.addRecipient(Message.RecipientType.TO, toAddress[i]);
                                 message.setSubject(subject);
                                 message.setText(body);
-                                Transport transport = session.getTransport("smtp");
+                                transport = session.getTransport("smtp");
                                 transport.connect(host, from, pass);
-                                transport.sendMessage(message, message.getAllRecipients());
-                                transport.close();
-                                PreparedStatement pr = con.prepareStatement("UPDATE " + table_name + " SET is_sended=? LIMIT 1");
-                                pr.setBoolean(1, true);
-                                pr.executeUpdate();
                             }
                         }
                     }
-                }catch (SQLException e) {
+                    try {
+                        for (int k = 0; k < message.getAllRecipients().length; k++)
+                            if (numberOfMeetings(all_emails, message.getAllRecipients()[k].toString()))
+                                all_emails.add(message.getAllRecipients()[k]);
+                    }
+                    catch (NullPointerException e){}
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
-        } catch (AddressException ae) {
-            ae.printStackTrace();
-        } catch (MessagingException me) {
+                catch (MessagingException me) {
+                    me.printStackTrace();
+                }
+            }
+                array = all_emails.toArray(new Address[all_emails.size()]);
+            try {
+                transport.sendMessage(message, array);
+                transport.close();
+            }
+            catch (NullPointerException e){}
+        }catch (MessagingException me) {
             me.printStackTrace();
         }
+
     }
-    private static boolean emailsEquals(String email1,String email2) {
-        Pattern address=Pattern.compile("([\\w\\.]+)@([\\w\\.]+\\.\\w+)");
-        Matcher match1=address.matcher(email1);
-        Matcher match2=address.matcher(email2);
-        if(!match1.find() || !match2.find()) return false; //Not an e-mail address? Already false
-        if(!match1.group(2).equalsIgnoreCase(match2.group(2))) return false; //Not same serve? Already false
-        switch(match1.group(2).toLowerCase()) {
+
+    private static boolean emailsEquals(String email1, String email2) {
+        Pattern address = Pattern.compile("([\\w\\.]+)@([\\w\\.]+\\.\\w+)");
+        Matcher match1 = address.matcher(email1);
+        Matcher match2 = address.matcher(email2);
+        if (!match1.find() || !match2.find()) return false; //Not an e-mail address? Already false
+        if (!match1.group(2).equalsIgnoreCase(match2.group(2))) return false; //Not same serve? Already false
+        switch (match1.group(2).toLowerCase()) {
             case "gmail.com":
-                String gmail1=match1.group(1).replace(".", "");
-                String gmail2=match2.group(1).replace(".", "");
+                String gmail1 = match1.group(1).replace(".", "");
+                String gmail2 = match2.group(1).replace(".", "");
                 return gmail1.equalsIgnoreCase(gmail2);
-            default: return match1.group(1).equalsIgnoreCase(match2.group(1));
+            default:
+                return match1.group(1).equalsIgnoreCase(match2.group(1));
         }
+    }
+
+    private static boolean numberOfMeetings(ArrayList<Address> l, String id) {
+        int count = 0;
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).toString().equals(id))
+                count++;
+        }
+        if (count > 1) return false;
+        else return true;
     }
 }
