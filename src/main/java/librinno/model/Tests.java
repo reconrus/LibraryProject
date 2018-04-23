af6e9e8c9e5b94b4782e51a4cddff7af96ad7387
DmitryKonev <Konev1999D@mail.ru>
2018-04-23 17:58:00 +0300

    Test1,initially and fields

--- src/main/java/librinno/model/Tests.java
+++ src/main/java/librinno/model/Tests.java
@@ -16,57 +16,39 @@ public class Tests {
     //creation of all needed information for tests
     Database db = new Database();
     Statement stmt = db.con.createStatement();
-    Librarian l = new Librarian("1", "1", "1", 1, "1", "1",null);
-    Book d1 = new Book("Introduction to Algorithms", "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest and Clifford Stein", "MIT Press", "Third edition", 5000, "b1", false, false, 2009, "In library");
-    Book d2 = new Book("Design Patterns: Elements of Reusable Object-Oriented Software", "Erich Gamma, Ralph Johnson, John Vlissides, Richard Helm", " Addison-Wesley Professional", "First edition", 1700, "b2", true, false, 2003, "In library");
-    //Book book3 = new Book("The Mythical Man-month", "Brooks,Jr., Frederick P", "Addison-Wesley Longman Publishing Co., Inc.", "Second edition", 1, "b3", false, true, 1995, "In library");
-    AV d3 = new AV("Null References: The Billion Dollar Mistake", ": Tony Hoare", 700, "av1");
+//    Librarian l = new Librarian("1", "1", "1", 1, "1", "1",null);
+    Book d1 = new Book("Introduction to Algorithms", "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest and Clifford Stein", "MIT Press", "Third edition", 5000, "Algorithms, Data Structures, Complexity, Computational Theory", false, false, 2009, "In library");
+    Book d2 = new Book("Algorithms + Data Structures = Programs", "Niklaus Wirth", "Prentice Hall PTR", "First edition", 5000, "Algorithms, Data Structures, Search Algorithms, Pascal", true, false, 1978, "In library");
+    Book d3 = new Book("The Art of Computer Programming", "Donald E. Knuth", "Addison Wesley Longman Publishing Co., Inc.", "Third edition", 5000, "Algorithms, Combinatorial Algorithms, Recursion", false, true, 1997, "In library");
+    //AV d3 = new AV("Null References: The Billion Dollar Mistake", ": Tony Hoare", 700, "av1");
     //AV av2 = new AV("Information Entropy", "Claude Shannon", 1, "av2");
     User p1 = new User("Sergey Afonso", "30001", "Via Margutta, 3", User.professor, "p1", "1@1.r");
     User p2 = new User("Nadia Teixeira", "30002", "Via Sacra, 13", User.professor, "p2", "2@2.r");
     User p3 = new User("Elvira Espindola", "30003", "Via del Corso, 22", User.professor, "p3", "sadasd@gmail.com");
     User s = new User("Andrey Velo", "30004", "Avenida Mazatlan 250", User.student, "s", "sasdasd@mail.com");
     User v = new User("Veronika Rama", "30005", "Stret Atocha, 27", User.vProfessor, "vp", "solovov305@gmail.com");
-
-
+    Admin admin1 = new Admin("Admin", "!", "1", 0, "Admin", "0", "r@r.ur" );
     public Tests() throws SQLException {
     }
 
     private void initially() throws SQLException{
-
-        l.addBook(d1.getTitle(), d1.getAuthor(), d1.getPublisher(), d1.getEdition(), d1.getPrice(), d1.getKeyWords(), d1.getBestseller(), d1.getReference(), d1.getYear(), 3);
-        l.addBook(d2.getTitle(), d2.getAuthor(), d2.getPublisher(), d2.getEdition(), d2.getPrice(), d2.getKeyWords(), d2.getBestseller(), d2.getReference(), d2.getYear(), 3);
-        l.addAV(d3.getTitle(), d3.getAuthor(), d3.getPrice(), d3.getKeyWords(), 2);
-        db.userCreation(p1);
-        db.userCreation(p2);
-        db.userCreation(p3);
-        db.userCreation(s);
-        db.userCreation(v);
-        d1.setId((Integer)db.isBookAlreadyExist(d1).get(1));
-        d2.setId((Integer)db.isBookAlreadyExist(d2).get(1));
-        d3.setId((Integer)db.isAVAlreadyExist(d3).get(1));
-        p1.setCardNumberAsString((Integer)db.isUserAlreadyExist(p1).get(1));
-        p2.setCardNumberAsString((Integer)db.isUserAlreadyExist(p2).get(1));
-        p3.setCardNumberAsString((Integer)db.isUserAlreadyExist(p3).get(1));
-        s.setCardNumberAsString((Integer)db.isUserAlreadyExist(s).get(1));
-        v.setCardNumberAsString((Integer)db.isUserAlreadyExist(v).get(1));
+        db.admin_creation(admin1);
     }
     public void tc1() throws SQLException {
         dump();
         initially();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM Copy");
+        Admin admin2=new Admin("admin","admin","admin",999,"Admin","admin","admin@mail.ru");
+        ResultSet rs = stmt.executeQuery("SELECT  * FROM users_of_the_library WHERE Type= 'Admin'");
         rs.last();
-        assert (rs.getRow() == 8);
-        rs = stmt.executeQuery("SELECT * FROM Users_of_the_library WHERE Card_number NOT IN (31,32)");
+        int admin_amount = rs.getRow();
+        assert (admin_amount==1);
+        db.admin_creation(admin2);
+        rs = stmt.executeQuery("SELECT  * FROM users_of_the_library WHERE Type= 'Admin'");
         rs.last();
-        assert (rs.getRow() == 5);
-        LocalDate date = LocalDate.parse("2018-03-05");
-        l.checkOutWithData(p1, d1.getId(), date);
-        l.checkOutWithData(p1, d2.getId(), date);
-        l.returnBook(d2.getId());
-        assert (l.fineWithDate(l.getAllCopiesTakenByUser(p1.getCard_number()).get(0).getId(), date)) == 0;
-        assert (l.getAllCopiesTakenByUser(p1.getCard_number()).get(0).getOverdue(p1, date)) == 0;
+        admin_amount=rs.getRow();
+        assert (admin_amount==1);
     }
+    /*
     public void tc2() throws SQLException {
         dump();
         initially();
